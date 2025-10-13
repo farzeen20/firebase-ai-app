68aac9895dc1164cae3fc219e573aa11b276fb3f
Farzeen Ahsan <farzeenahsan20@gmail.com>
2025-10-13 10:28:18 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'chart.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
